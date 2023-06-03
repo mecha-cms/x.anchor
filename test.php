8a3f582c151e0e2372273d7958701dc72c184fe1
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-03 21:29:21 +0700

    Update

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 
-$content = file_get_contents(__DIR__ . D . 'test');
+$content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo '<style>:target{background:#ff0}a[aria-hidden]::before{content:\'\00A7\'}</style>';
 echo x\anchor\content($content);
