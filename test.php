baa3aea05e1da86f22fef8c06083b8b93916e06f
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-06-04 11:46:19 +0700

    Rename Hook

--- test.php
+++ test.php
@@ -3,6 +3,6 @@
 $content = file_get_contents(__DIR__ . D . 'test.txt');
 
 echo '<style>:target{background:#ff0}a[aria-hidden]::before{content:\'\00A7\'}</style>';
-echo x\anchor\content($content);
+echo x\anchor\page\content($content);
 
 exit;
